0e0cbe271d68a54771f7724e0e816ea9d51c9133
kuaker <sbumission@gmail.com>
2021-01-31 17:21:43 -0300

    feat/cucumberTests

--- cypress/integration/BDD/ecommerce.feature
+++ cypress/integration/BDD/ecommerce.feature
@@ -3,6 +3,6 @@ Feature: End to End Ecommerce validation
   Regresion
 
 Scenario: Buy products
-Given I open demoBlazer
+Given I open ecommerce
 When I select the products
 Then I add the products
---
+++ cypress/integration/common/ecommStepDef.js
@@ -0,0 +1,17 @@
+import { Given,When, Then } from "cypress-cucumber-preprocessor/steps";
+import TasksSellPage from "../storeProject/tasks/TasksSellPage";
+import TasksHomePage from "../storeProject/tasks/TasksHomePage";
+
+const url = Cypress.env("baseUrl");
+const tasksellpage = new TasksSellPage();
+const taskhomepage = new TasksHomePage();
+
+Given("I open ecommerce", function () {
+	cy.visit(url);
+});
+When("I select the products", function () {
+	taskhomepage.taskSelectAProduct();
+});
+Then("I add the products", function () {
+	tasksellpage.taskAddAProduct();
+});
